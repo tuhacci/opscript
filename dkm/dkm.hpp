9ce854909d3724fcd6392caade8afe42b6d69b7d
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-03-17 13:05:24 +0000

    Update docs. [ci skip]

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -40,4 +40,8 @@ T distance_squared(const std::array<T, N>& point_a, const std::array<T, N>& poin
 
 template <typename T, size_t N>
 T distance(const std::array<T, N>& point_a, const std::array<T, N>& point_b) {
-	return std::sqrt(distance_squared(point_a, po
+	return std::sqrt(distance_squared(point_a, point_b));
+}
+
+/*
+Calc
