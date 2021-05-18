8548434cc0671ebd2065849beeb749b71229749a
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-05-18 19:31:34 +0000

    msm: vidc: Query the core on VIDIOC_G_FMT

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -64,4 +64,4 @@ std::vector<T> closest_distance(
 }
 
 /*
-This is an alternate initialization method based on the [kmeans++](https://en.wikipedia.org/wiki
+This is an alternate initialization method based on the [kmeans++](https://en.wikipedia.org/wiki/K-means%2B%2B)
