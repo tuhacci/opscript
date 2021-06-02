9286f5e4b67f028cfac0ddf28e9e321c95f710f0
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-06-02 12:20:54 +0000

    Add CHANGELOG.

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -71,4 +71,4 @@ template <typename T, size_t N>
 std::vector<std::array<T, N>> random_plusplus(const std::vector<std::array<T, N>>& data, uint32_t k, uint64_t seed) {
 	assert(k > 0);
 	assert(data.size() > 0);
-	using input_size_t = typename std::arr
+	using input_size_t = typename std::array<T, N>::siz
