5feb43faba91814018982b5adce3c8869f505437
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-06-04 14:15:12 +0000

    list view

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -72,4 +72,5 @@ std::vector<std::array<T, N>> random_plusplus(const std::vector<std::array<T, N>
 	assert(k > 0);
 	assert(data.size() > 0);
 	using input_size_t = typename std::array<T, N>::size_type;
-	std::vector<std::array<T, 
+	std::vector<std::array<T, N>> means;
+	// U
