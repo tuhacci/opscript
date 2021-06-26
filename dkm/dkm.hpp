e1c7d53493f213e4efefcfcace860e43b4fd02a7
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-06-26 14:00:22 +0000

    Preparing changelog for upload

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -75,4 +75,6 @@ std::vector<std::array<T, N>> random_plusplus(const std::vector<std::array<T, N>
 	std::vector<std::array<T, N>> means;
 	// Using a very simple PRBS generator, parameters selected according to
 	// https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
-	std::linear_congruential_engine<uint64_t, 6364136223846793005, 1442695040888963407, UINT64_MAX> rand_engine(see
+	std::linear_congruential_engine<uint64_t, 6364136223846793005, 1442695040888963407, UINT64_MAX> rand_engine(seed);
+
+	// Select fir
