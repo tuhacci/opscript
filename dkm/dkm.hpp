6a645eda89c56d24cd9fb05aedff5e55b78bfc24
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-08-05 05:42:45 +0000

    Revert "Initial Commit"

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -86,4 +86,4 @@ std::vector<std::array<T, N>> random_plusplus(const std::vector<std::array<T, N>
 	for (uint32_t count = 1; count < k; ++count) {
 		// Calculate the distance to the closest mean for each data point
 		auto distances = details::closest_distance(means, data);
-		// Pick a random point weighte
+		// Pick a random point weighted by the dista
