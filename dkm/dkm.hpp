367c07271fc753a2c62003393c74d06366e255d4
tuhacci <96789280+tuhacci@users.noreply.github.com>
2021-12-01 10:28:17 +0000

    Updated the sphinx feedstock.

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -123,4 +123,4 @@ Calculate the index of the mean each data point is closest to (euclidean distanc
 */
 template <typename T, size_t N>
 std::vector<uint32_t> calculate_clusters(
-	const std::vector<std::array<T, N>>& data, const
+	const std::vector<std::array<T, N>>& data, const std::vector<std::array
