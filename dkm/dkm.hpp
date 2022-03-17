70b3f9cd476518dab54534ad6510fc211bfcdcd7
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-03-17 10:41:23 +0000

    Cleaned up code a bit

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -165,4 +165,4 @@ std::vector<T> deltas(
 	const std::vector<std::array<T, N>>& old_means, const std::vector<std::array<T, N>>& means)
 {
 	std::vector<T> distances;
-	dist
+	distances.reserve(mean
