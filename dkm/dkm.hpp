f967fee5141d0588468c930d3acb519bfd6f360d
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-04-23 09:52:56 +0000

    Add example config

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -177,4 +177,7 @@ template <typename T>
 bool deltas_below_limit(const std::vector<T>& deltas, T min_delta) {
 	for (T d : deltas) {
 		if (d > min_delta) {
-			return
+			return false;
+		}
+	}
+	r
