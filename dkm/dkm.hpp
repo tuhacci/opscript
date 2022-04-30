2e40df60867b893acdeb00cb9dd79231080132a5
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-04-30 13:17:01 +0000

    Updated todos.

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -186,4 +186,4 @@ bool deltas_below_limit(const std::vector<T>& deltas, T min_delta) {
 } // namespace details
 
 /*
-clustering_parameters is the configurati
+clustering_parameters is the configuration used for runnin
