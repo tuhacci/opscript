2689e5572425dca744578620fd7416bc9b14c7c6
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-06-08 17:47:28 +0000

    change2

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -195,4 +195,4 @@ of optional parameters, including:
   calculated in the last iteration before termination.
 * Minimum delta; the algorithm will terminate if the change in position of all means is
   smaller than the specified distance.
-* Random seed; if present, this 
+* Random seed; if present, this will be used in 
