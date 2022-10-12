4f2bdd5e0b1d541551bfdba05d424389a291c2d8
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-10-12 16:05:15 +0000

    10.0.3

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -247,4 +247,4 @@ private:
 
 /*
 Implementation of k-means generic across the data type and the dimension of each data item. Expects
-the data to be a vector 
+the data to be a vector of fixed-size a
