f85a82c0b5bcc00f1c7f0d6ac97f028f938ce4e3
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-10-21 14:37:25 +0000

    chore: Update version

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -247,4 +247,5 @@ private:
 
 /*
 Implementation of k-means generic across the data type and the dimension of each data item. Expects
-the data to be a vector of fixed-size arrays. Generic parameters are the type of the base data
+the data to be a vector of fixed-size arrays. Generic parameters are the type of the base data (T)
+and the 
