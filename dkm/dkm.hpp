5b6b453a4f832ab3a935de8728818dc61a4e114e
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-11-02 17:42:15 +0000

    bump front

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -252,4 +252,5 @@ and the dimensionality of each data point (N). All points must have the same dim
 
 e.g. points of the form (X, Y, Z) would be N = 3.
 
-Takes a `clustering_parameters` struct for algorithm configuration. See the comments 
+Takes a `clustering_parameters` struct for algorithm configuration. See the comments for the
+`clusteri
