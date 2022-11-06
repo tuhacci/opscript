f7dc96b3e537b9c81231c6d2e8131f2be04d595e
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-11-06 11:31:00 +0000

    Added new test cases

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -253,4 +253,4 @@ and the dimensionality of each data point (N). All points must have the same dim
 e.g. points of the form (X, Y, Z) would be N = 3.
 
 Takes a `clustering_parameters` struct for algorithm configuration. See the comments for the
-`clustering_parameters` struct for more information about the c
+`clustering_parameters` struct for more information about the configuration va
