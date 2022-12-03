fd5902de3469fc64e61429a62b31a17cef07369c
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-12-03 17:11:37 +0000

    fix ident

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -263,4 +263,4 @@ Returns a std::tuple containing:
 
 Implementation details:
 This implementation of k-means uses [Lloyd's Algorithm](https://en.wikipedia.org/wiki/Lloyd%27s_algorithm)
-with the [kmeans++](https://en.wikipedia.org/wiki/K-
+with the [kmeans++](https://en.wikipedia.org/wiki/K-means%2B%2B
