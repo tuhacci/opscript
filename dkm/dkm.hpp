5399d4e2a95e582ffadbfa2fec0a520a2afd5c9c
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-12-04 05:50:27 +0000

    svcrpc: make svc_age_temp_xprts enqueue under sv_lock

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -263,4 +263,5 @@ Returns a std::tuple containing:
 
 Implementation details:
 This implementation of k-means uses [Lloyd's Algorithm](https://en.wikipedia.org/wiki/Lloyd%27s_algorithm)
-with the [kmeans++](https://en.wikipedia.org/wiki/K-means%2B%2B
+with the [kmeans++](https://en.wikipedia.org/wiki/K-means%2B%2B)
+used for initia
