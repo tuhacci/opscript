879d324366a39e203351e4d0eef514aedb57b9cd
tuhacci <96789280+tuhacci@users.noreply.github.com>
2022-12-18 11:05:22 +0000

    formatting

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -269,4 +269,5 @@ used for initializing the means.
 */
 template <typename T, size_t N>
 std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
-	const std::vector<std::array<T, N>>& data, const clustering_parameters<T>& paramete
+	const std::vector<std::array<T, N>>& data, const clustering_parameters<T>& parameters) {
+	static
