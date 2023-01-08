b01b4a39106523bd6503939a07bb33fadfdd21a8
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-01-08 08:35:01 +0000

    primo commit

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -271,4 +271,5 @@ template <typename T, size_t N>
 std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
 	const std::vector<std::array<T, N>>& data, const clustering_parameters<T>& parameters) {
 	static_assert(std::is_arithmetic<T>::value && std::is_signed<T>::value,
-		"kmeans_lloyd requires the template parameter T to be a signed arithmetic type (e.g. float, doubl
+		"kmeans_lloyd requires the template parameter T to be a signed arithmetic type (e.g. float, double, int)");
+	as
