08f185b70083a5e1710744739fa254fdc5b17fde
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-01-18 17:46:23 +0000

    Translated using Weblate (German)

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -275,4 +275,5 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
 	assert(parameters.get_k() > 0); // k must be greater than zero
 	assert(data.size() >= parameters.get_k()); // there must be at least k data points
 	std::random_device rand_device;
-	uint64_t seed = parameters.has_random_seed() ? parameters.get_random_seed() : rand_de
+	uint64_t seed = parameters.has_random_seed() ? parameters.get_random_seed() : rand_device();
+	
