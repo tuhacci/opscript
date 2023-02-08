7ca367a42fe9afa7189699e910c7f75839dc75cc
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-02-08 18:37:19 +0000

    Delete .pydevproject

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -279,4 +279,5 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
 	std::vector<std::array<T, N>> means = details::random_plusplus(data, parameters.get_k(), seed);
 
 	std::vector<std::array<T, N>> old_means;
-	std::vector<std::array<T, N>> old_old_mea
+	std::vector<std::array<T, N>> old_old_means;
+	std::vector<u
