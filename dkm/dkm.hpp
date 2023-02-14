63f34aec965eb8cdc0e08a45440652f4cc0d85c5
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-02-14 08:50:47 +0000

    Delete main.yml

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -281,4 +281,4 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
 	std::vector<std::array<T, N>> old_means;
 	std::vector<std::array<T, N>> old_old_means;
 	std::vector<uint32_t> clusters;
-	// Calculate ne
+	// Calculate new means u
