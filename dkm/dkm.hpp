d22e39eae5e833a2b8a9cd1140c276922d3f3793
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-02-22 20:20:28 +0000

    goimports

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -286,4 +286,4 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
 	do {
 		clusters = details::calculate_clusters(data, means);
 		old_old_means = old_means;
-		ol
+		old_means = means;
