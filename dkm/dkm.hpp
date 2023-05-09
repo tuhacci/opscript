456a28e11cbc4109895a337c19a578b732488776
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-05-09 18:34:57 +0000

    xhci: correct burst count field for isoc transfers on 1.0 xhci hosts

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -302,4 +302,5 @@ Any code still using this signature should move to the version of this function 
 `clustering_parameters` struct for configuration.
 */
 template <typename T, size_t N>
-std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmean
+std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
+	const std::vect
