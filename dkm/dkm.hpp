e11a6dbf764c6b9be2f491a36961bcca0baf1cfd
tuhacci <96789280+tuhacci@users.noreply.github.com>
2023-05-25 22:39:20 +0000

    Создан проект PyCharm

--- dkm/dkm.hpp
+++ dkm/dkm.hpp
@@ -307,4 +307,6 @@ std::tuple<std::vector<std::array<T, N>>, std::vector<uint32_t>> kmeans_lloyd(
 	uint64_t max_iter = 0, T min_delta = -1.0) {
 	clustering_parameters<T> parameters(k);
 	if (max_iter != 0) {
-		parameters.set_max_iteration(max_
+		parameters.set_max_iteration(max_iter);
+	}
+	if (
